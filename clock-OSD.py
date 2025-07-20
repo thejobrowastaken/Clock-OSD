aac50422ee11ca6e83cca08fb68612dd424de59f
thejobrowastaken <sharad.r.ken@gmail.com>
2025-07-20 17:40:19 +0530

    Update clock-OSD.py

--- clock-OSD.py
+++ clock-OSD.py
@@ -1,14 +1,18 @@
 from tkinter import *
 from time import strftime
 
+def killer():
+    quit()
+
 menu = Tk()
 menu.config(bg="#8db3f0")
+a = "Clock"
+menu.title(a)
 x_screenreso = menu.winfo_screenmmwidth()
 y_screenreso = menu.winfo_screenmmheight()
 xreso = x_screenreso/2 + 565
 yreso = y_screenreso/2 + 100
 menu.geometry(f"200x400+{int(xreso)}+{int(yreso)}")
-
 
 def DigitalClock():
     root = Tk()
@@ -30,8 +34,9 @@ def DigitalClock():
     lb.pack()
     clocktime()
     root.mainloop()
-def quit():
-    menu.destroy()
+
+    
+    
 
 
 menu.rowconfigure((0,1,2,3,4), weight=2)
@@ -39,7 +44,7 @@ menu.columnconfigure((0,1,2), weight=2)
 lb = Label(menu, text="Da Menu", fg="white", bg="#8db3f0", font=("Arial", 25, "bold"))
 settingbtn = Button(menu, text="Settings", fg="white", bg="#8db3f0",font=("Arial", 25, "bold"))
 clockbtn = Button(menu, text="  Clock  ", fg="white", bg="#8db3f0",font=("Arial", 25, "bold"), command=DigitalClock)
-quitbtn = Button(menu, text="quit", fg="white", bg="#8db3f0",font=("Arial", 25, "bold"), command=quit)
+quitbtn = Button(menu, text="quit", fg="white", bg="#8db3f0",font=("Arial", 25, "bold"), command=killer)
 note = Label(menu, text="Note:only clock works for now", fg="white", bg="#8db3f0",font=("Arial", 10, "italic"))
 
 lb.grid(row=0, column=1, sticky="n", pady=10)
@@ -48,6 +53,7 @@ clockbtn.grid(row=2, column=1, sticky="new")
 quitbtn.grid(row=3, column=1,sticky="new")
 note.grid(row=4, columnspan=3)
 menu.mainloop()
+
 
 
 
