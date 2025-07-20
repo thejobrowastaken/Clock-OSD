e272dfc6d31835a26f9c7260745c86c80f97b145
thejobrowastaken <sharad.r.ken@gmail.com>
2025-07-20 23:51:45 +0530

    Update Experiments.py

--- Experiments.py
+++ Experiments.py
@@ -1,40 +1,67 @@
 #code to experiment with
 
 from tkinter import *
+from tkinter import colorchooser
+from time import strftime, sleep
 
-root = Tk()
-root.config(bg="grey")
-root.geometry('800x800')
-root.title("grid test")
+#Items requiring outside of mainloop
+def kill():
+    quit()
 
-def float():
-    lb1.config(text="0.1123")
+#Variables 
+bgColour='#b0c3e8'
+fgColour = '#020c21'
+Font_Of_Title = ("Arial", 20, "bold")
+Common_Font = ("Arial", 15, "bold")
 
-def int():
-    lb1.config(text=2020)
 
-def string():
-    lb1.config(text="Taiwan is a country")
+Root = Tk()
+Root.geometry('400x400')
+Root.title("Clock V2")
+Root.config(bg=bgColour)
+#Root Info
 
-def textpick():
-    a = Txt.get("1.0",'end-1c')
-    a = str(a)
-    lb1.config(text=a)
+#Func storage
+def DigitalClock(): 
+    colorpickervalue = '#8db3f0'
+    root = Tk()
+    x_screenreso = root.winfo_screenmmwidth()
+    y_screenreso = root.winfo_screenmmheight()
+    root.resizable(False,False)
+    root.config(bg='black')
+    xresoclock = x_screenreso/2 + 1360  
+    yresoclock = y_screenreso/2 - 150
+    root.geometry(f"320x100+{int(xresoclock)}+{int(yresoclock)}")
+    root.attributes('-topmost', True)
+    root.wm_attributes('-transparent', 'black')
+    root.wm_overrideredirect(1)
+    def clocktime():
+        a = strftime('%H:%M:%S')
+        lb.config(text=a)
+        lb.config(fg=colorpickervalue)
+        lb.after(1000,clocktime)
+    lb = Label(root, width=25, height=1, bg='black', fg=colorpickervalue, font=("Arial", 35, 'bold'))
+    lb.pack()
+    clocktime()
+    root.mainloop()
 
-root.rowconfigure((0,1,2,3,4,5,6,7,8), weight=2)
-root.columnconfigure((0,1,2,3,4,5,6,7,8), weight=2)
+def Settings():         #I dont know what im doing but i know itll work barely
+    Title.config(text="Setup")
+    Clock.config(text="Colour Picker")
+    Customize.destroy()
 
-lb1 = Label(text="Hello World", font=("Arial", 10, 'bold'), bg="blue", fg="#8db8f0")
-bt1 = Button(text="float", font=("Arial", 10, 'bold'), bg="Black", fg="#8db8f0", command=float)
-bt2 = Button(text="Int", font=("Arial", 10, 'bold'), bg="Black", fg="#8db8f0", command=int)
-bt3 = Button(text="String", font=("Arial", 10, 'bold'), bg="Black", fg="#8db8f0", command=string)
-Txt = Text(root, bg="black", fg="#00ff44", font=("Arial", 10, "italic"))
-txtbtn = Button(root, bg="Yellow", fg="#8db8f0",font=("Arial", 10, 'bold'), command=textpick)
+Root.rowconfigure((0,1,2,3), weight=2)
+Root.columnconfigure((0,1,2,3), weight=2)
+#Rows and Columns
 
-lb1.grid(row=0, column=4,  sticky="nsew", padx=20, pady=20)
-bt1.grid(row=4, column=0,  sticky="nswe", columnspan=2, padx=20, pady=20)
-bt2.grid(row=5, column=0,  sticky="nswe", columnspan=2, padx=20, pady=20)
-bt3.grid(row=6, column=0,  sticky="nswe", columnspan=2, padx=20, pady=20)
-Txt.grid(row=3, column=4, sticky="ew")
-txtbtn.grid(row=4, column=4, sticky="ew")
-root.mainloop()
+#Titles
+Title = Label(Root, text="ClockMenu", bg=bgColour, font=("Arial", 20, "bold"), fg=fgColour)
+Clock = Button(Root, text="Clock", bg=bgColour, font=Common_Font, fg=fgColour, command=DigitalClock)
+Customize = Button(Root, text="Customization", bg=bgColour, font=Common_Font, fg=fgColour, command=Settings)
+quitbtn = Button(Root, text="Quit", bg=bgColour, font=Common_Font, fg=fgColour, command=kill)
+
+Title.grid(row=0, column=1, columnspan=2, sticky="wne", pady=20)
+Clock.grid(row=1, column=1, columnspan=2, sticky='wne')
+Customize.grid(row=2, column=1, columnspan=2, sticky='wne')
+quitbtn.grid(row=3, column=1, columnspan=2, sticky='wne')
+Root.mainloop()
